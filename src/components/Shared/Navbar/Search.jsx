87c89f9d06f0b84b778dd1ea44810c69e9e5ed95
zabaidulbd <zislam5622@gmail.com>
2024-01-23 12:07:26 +0600

    add navbar

---
+++ src/components/Shared/Navbar/Search.jsx
@@ -0,0 +1,14 @@
+
+const Search = () => {
+    return (
+        <div className='border-[1px] w-full md:w-auto py-4 rounded shadow-sm hover:shadow-md transition cursor-pointer'>
+            <div className='flex flex-row items-center justify-between'>
+                <div className='text-sm pl-6 pr-6 text-gray-800 flex flex-row items-center gap-3'>
+                    <div className='hidden sm:block'>Search Your Desire House from House Hunter</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
